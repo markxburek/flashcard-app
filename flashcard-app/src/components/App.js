7739cbed3bdbb713313cb3ca003d401d126a6b65
markxburek <markxburek@gmail.com>
2020-04-17 22:39:30 -0500

    Array loses one element each turn

--- flashcard-app/src/components/App.js
+++ flashcard-app/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
  
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk'
+// import thunk from 'redux-thunk'
 //import {getNextFlashCard} from './../actions'
 import getNextFlashCardReducer from './../reducers/getNextFlashCardReducer'
 import buttonStateReducer from './../reducers/buttonStateReducer'
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
   buttonPanel: buttonStateReducer,
   flashCard: getNextFlashCardReducer
 });
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer );
 //const store = createStore(getNextFlashCardReducer);
 //const Provider = ReactRedux.Provider;
 
--- flashcard-app/src/reducers/getNextFlashCardReducer.js
+++ flashcard-app/src/reducers/getNextFlashCardReducer.js
@@ -1,34 +1,77 @@
-import {flashCardData, NUMBER_OF_FLASHCARDS} from '../flashCardData';
+import { flashCardData, NUMBER_OF_FLASHCARDS } from '../flashCardData';
+
+//let remainingIndexArray = [...Array(NUMBER_OF_FLASHCARDS).keys()];
+
+
 
 const getNextIndex = () => Math.floor(Math.random() * NUMBER_OF_FLASHCARDS);
-const getNextCard = () => flashCardData[getNextIndex()  ].front;
+// const getNextIndex = () => {
+//     let index = Math.floor(Math.random() * NUMBER_OF_FLASHCARDS); 
+//     while( flashCardData[index ].timesShown !== 0){
+//         index = Math.floor(Math.random() * NUMBER_OF_FLASHCARDS);
+//     }
+//     return index;
+
+// }
+
+let getRandomIndex = (indexArray) => {
+    return Math.floor(Math.random() * (indexArray.length))
+}
+
+const removeElement = index => indexArray.filter(x => indexArray.indexOf(x) !== index)
+
+//initialize on startup 
+let indexArray = [...Array(flashCardData.length).keys()]
+
+
 
 const getFrontOfFlashCard = (index) => flashCardData[index].front;
 const getBackOfFlashCard = (index) => flashCardData[index].back
 const initialIndex = getNextIndex();
 
 
-const initialState = {index: initialIndex,  
-                        frontCard: getFrontOfFlashCard(initialIndex), 
-                        backCard: getBackOfFlashCard(initialIndex),
-                        viewing: getFrontOfFlashCard(initialIndex)
+const initialState = {
+    indexArray: indexArray,
+    index: initialIndex,
+    frontCard: getFrontOfFlashCard(initialIndex),
+    backCard: getBackOfFlashCard(initialIndex),
+    viewing: getFrontOfFlashCard(initialIndex)
 };
 
+
+//const removeElement = index => remainingIndexArray.filter(  element => element !== index); 
+
 const getNextFlashCardReducer = (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case "GET_NEXT_FLASHCARD":
+
             let index = getNextIndex();
             let frontCard = getFrontOfFlashCard(index);
+            let backCard = getBackOfFlashCard(index);
+            let indexArray = state.indexArray;
+            indexArray.pop();
 
-            return {index: index, 
-                frontCard: frontCard,
-                backCard: getBackOfFlashCard(index),
-                viewing: frontCard
+
+            return {
+                indexArray: indexArray ,
+                index: index,
+                frontCard: state.frontCard,
+                backCard: backCard,
+                viewing: indexArray.toString()
+                //viewing: Object.keys(state)
             };
-         
+
         case "GET_FLASHCARD_ANSWER":
-            return { viewing: state.backCard}    
-             
+            return Object.assign(
+                state,
+                {
+                    
+                    viewing: state.backCard
+                    //viewing: Object.keys(state)
+                }
+
+            )
+
         default:
             return state;
 
