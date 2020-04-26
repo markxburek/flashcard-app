9ac6ff2a1de24a7815c20e3b54c9acb8175f71d0
markxburek <markxburek@gmail.com>
2020-04-25 21:48:20 -0500

    AddCardScreen has inputs for front and back

--- flashcard-app/src/components/AddCardScreen.js
+++ flashcard-app/src/components/AddCardScreen.js
@@ -1,12 +1,58 @@
 import React from 'react'
 
-const AddScreen = (props) => {
-    return (
-        <div>
-            <h2>Add Screen </h2>
-            
-        </div>
-    )
-}
+class AddScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            front: '',
+            submitedFront: '',
+            back: '',
+            submitedBack:''
+        };
+        this.handleChangeFront = this.handleChangeFront.bind(this);
+        this.handleChangeBack = this.handleChangeBack.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChangeFront(event) {
+        this.setState({
+            front: event.target.value
+        });
+    }
+    handleChangeBack(event) {
+        this.setState({
+            back: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({
+            front: '',
+            submitedFront: this.state.front,
+            submitedBack: this.state.back
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h3> Add Card</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <p>Front</p>
+
+                    <input value={this.state.front} onChange={this.handleChangeFront} />
+                    <p>Back</p>
+                    <input value={this.state.back} onChange={this.handleChangeBack} /><br/>
+
+
+                    <button type='submit'>Submit!</button>
+                </form>
+
+                <p>You added:</p>
+                <h1>{this.state.submitedFront}</h1>
+                <h1>{this.state.submitedBack}</h1>
+            </div>
+        );
+    }
+};
 
 export default AddScreen;
