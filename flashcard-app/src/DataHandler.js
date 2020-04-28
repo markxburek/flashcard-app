3e91e58d26b646e38041793806f4d1d0f6b9a570
markxburek <markxburek@gmail.com>
2020-04-27 23:12:34 -0500

    Added three new properties to data

--- flashcard-app/src/DataHandler.js
+++ flashcard-app/src/DataHandler.js
@@ -4,9 +4,9 @@ import {flashCardData} from './flashCardData'
 export class DataHandler {
     constructor(){
         this.data = [ 
-            {DECK_NAME: "deck1", NEW: 10, LEARNING: 25, TO_REVIEW: 70},
-            {DECK_NAME: "deck2", NEW: 15, LEARNING: 13, TO_REVIEW: 42},
-            { DECK_NAME: "deck3", NEW: 3, LEARNING: 53, TO_REVIEW: 100 }
+            {DECK_NAME: "deck1", NEW: 10, LEARNING: 25, TO_REVIEW: 70, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
+            {DECK_NAME: "deck2", NEW: 15, LEARNING: 13, TO_REVIEW: 42, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
+            {DECK_NAME: "deck3", NEW: 3, LEARNING: 53, TO_REVIEW: 100, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0 }
         ]; 
     }
 
