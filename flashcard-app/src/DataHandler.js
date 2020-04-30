89bf61985de4a941e966b4c84a0970afc27b2e0d
markxburek <markxburek@gmail.com>
2020-04-30 01:13:04 -0500

    Added two new decks

--- flashcard-app/src/DataHandler.js
+++ flashcard-app/src/DataHandler.js
@@ -1,6 +1,7 @@
 //Intermediary between components and data
-import {flashCardData} from './flashCardData'
+import {flashCardData, multiCardData, vocabData} from './flashCardData'
 
+ 
 const DECKDATA = "deckData"
 const SELECTED_DECK_INDEX = 0;
 
@@ -8,8 +9,8 @@ export class DataHandler {
     constructor(){
         this.data = [ 
             {DECK_NAME: "initialDeck",deckData: flashCardData , NEW: 5, LEARNING: 5, TO_REVIEW: 5, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
-            {DECK_NAME: "deck2", NEW: 15, LEARNING: 13, TO_REVIEW: 42, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
-            {DECK_NAME: "deck3", NEW: 3, LEARNING: 53, TO_REVIEW: 100, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0 }
+            {DECK_NAME: "multiplication", deckData: multiCardData, NEW: 15, LEARNING: 13, TO_REVIEW: 42, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
+            {DECK_NAME: "vocabulary", deckData: vocabData, NEW: 3, LEARNING: 53, TO_REVIEW: 100, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0 }
         ];
         this.selectedDeckData = this.getDeckData(SELECTED_DECK_INDEX); 
 
