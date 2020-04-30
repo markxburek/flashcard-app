322018160dac8d70059834144c0612c57b3b81a7
markxburek <markxburek@gmail.com>
2020-04-30 01:28:54 -0500

    Fixed multCardData typo

--- flashcard-app/src/DataHandler.js
+++ flashcard-app/src/DataHandler.js
@@ -1,5 +1,5 @@
 //Intermediary between components and data
-import {flashCardData, multiCardData, vocabData} from './flashCardData'
+import {flashCardData, multCardData, vocabData} from './flashCardData'
 
  
 const DECKDATA = "deckData"
@@ -9,12 +9,17 @@ export class DataHandler {
     constructor(){
         this.data = [ 
             {DECK_NAME: "initialDeck",deckData: flashCardData , NEW: 5, LEARNING: 5, TO_REVIEW: 5, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
-            {DECK_NAME: "multiplication", deckData: multiCardData, NEW: 15, LEARNING: 13, TO_REVIEW: 42, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
+            {DECK_NAME: "multiplication", deckData: multCardData, NEW: 15, LEARNING: 13, TO_REVIEW: 42, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0},
             {DECK_NAME: "vocabulary", deckData: vocabData, NEW: 3, LEARNING: 53, TO_REVIEW: 100, CORRECT: 0, INCORRECT: 0, TIMES_SHOWN: 0 }
         ];
         this.selectedDeckData = this.getDeckData(SELECTED_DECK_INDEX); 
 
         this.getSelectedDeckData = this.getSelectedDeckData.bind(this);
+    }
+
+    getDeckByName(deckName){
+        let index =  this.data.findIndex( deck => deck.DECK_NAME === deckName);
+        return this.data[index];
     }
  
 
